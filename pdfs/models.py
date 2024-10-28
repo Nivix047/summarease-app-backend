174acc3fbcc900d29c05b233e957334a12f1ee14
Daniel Xu <xudh47@gmail.com>
2024-10-27 20:49:03 -0700

    Refactored user model and pdfs model to use UUID

--- pdfs/models.py
+++ pdfs/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+import uuid
 
 User = get_user_model()
 
 class PDFDocument(models.Model):
+  id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
   user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='pdf_documents')
   title = models.CharField(max_length=255)
   file= models.FileField(upload_to='pdfs/')
