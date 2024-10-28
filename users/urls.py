81c866b02c7f4f2fca2f8a2f66b0408af1879e0d
Daniel Xu <xudh47@gmail.com>
2024-10-27 19:07:24 -0700

    fixed url bug for pdf_summarizer

--- users/urls.py
+++ users/urls.py
@@ -5,11 +5,11 @@ from pdfs.views import PDFDocumentViewSet
 
 router = DefaultRouter()
 router.register(r'', CustomUserViewSet, basename='user')
-router.register(r'pdfs', PDFDocumentViewSet, basename='pdfdocument')
 
 urlpatterns = [
     path('register/', CustomUserViewSet.as_view({'post': 'register'}), name='register'),  # Custom route for registration
     path('login/', login_view, name='login'),
+    path('pdfs/', PDFDocumentViewSet.as_view({'post': 'create'}), name='upload_pdf'),
     path('', include(router.urls)),  # All other user routes
 ]
  
